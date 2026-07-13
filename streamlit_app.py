5328fc12e10f46aca2986ace5b66b8bc17bc9fb7
donnyhardika <donny.hardika@gmail.com>
2026-07-13 13:38:12 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+from app.utils import model_helper
+st.header("Fraud Claim Predictor")
+file = st.file_uploader("Upload data yang ingin diprediksi (csv )", type=".csv")
+if file:
+    df_file = pd.read_csv(file)
+    st.dataframe(df_file)
+    predict = st.button("Predict !")
+    if predict:
+        result = model_helper.predict(df_file)
+        predict_proba = result["prob"]
+        predict_class = result["class"]
+        result_df = pd.DataFrame({
+            "claim_number": df_file["claim_number"],
+            "prediction": predict_class,
+            "probability": predict_proba
+        })
+        st.success("Hore! Prediksi berhasil")
+        st.dataframe(result_df)
